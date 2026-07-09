180b0fe17b49690ebb89c43be2d2275604a2bfb4
Harshika Malhotra <malhotra.harshikaa@gmail.com>
2026-07-09 22:25:01 +0530

    Add login button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
 import foodCartLogo from "../../public/images/foodCartLogo.png";
 import { FaCartArrowDown } from "react-icons/fa";
+import { useState } from "react";
 
 const Header = () => {
+    const [login, setLogin] = useState(false);
+
     return (
         <div className="header">
             <div className="logo-container" >
@@ -12,9 +15,14 @@ const Header = () => {
             <div className="nav-items">
                 <ul>
                     <li>Home</li>
-                    <li>ABout Us</li>
+                    <li>About Us</li>
                     <li>Contact Us</li>
                     <li><FaCartArrowDown /></li>
+                    {/* toggle login logout */}
+                    <button
+                        className="login"
+                        onClick={() => setLogin(!login)}
+                    >{login ? "Logout" : "Login"}</button>
                 </ul>
             </div>
         </div>
